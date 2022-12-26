8852fed9dc401bb1806556d4317bfbb3290f5e90
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2022-12-26 17:46:45 +0200

    add username to info messages

--- functions/bot/handlers/handle-teams-quantity-button-click.js
+++ functions/bot/handlers/handle-teams-quantity-button-click.js
@@ -1,28 +1,27 @@
-const { store, resetStore } = require('../store');
-const { replies, buttons, getButtonText } = require('../helpers');
-const handleError = require('./handle-error');
+const { store, resetStore } = require('../store')
+const { replies, buttons, getButtonText } = require('../helpers')
+const handleError = require('./handle-error')
 
 module.exports = async function handleTeamsQuantityButtonClick(ctx) {
   try {
-    if (!store.splitVariant)
-      return await ctx.reply(replies.firstChooseSplitVariantReply, buttons.splitVariantButtons);
+    if (!store.splitVariant) return await ctx.reply(replies.firstChooseSplitVariantReply, buttons.splitVariantButtons)
 
-    const { first_name, last_name } = ctx.callbackQuery.from;
-    store.teamsQuantity = Number(ctx.callbackQuery.data[0]);
-    store.list = 'players';
+    const { first_name, last_name, username } = ctx.callbackQuery.from
+    store.teamsQuantity = Number(ctx.callbackQuery.data[0])
+    store.list = 'players'
 
-    for (let i = 1; i <= store.teamsQuantity; i++) store.teamsData[i] = [];
+    for (let i = 1; i <= store.teamsQuantity; i++) store.teamsData[i] = []
 
     let reply = `
-<i>ℹ️ ${first_name} ${last_name ? last_name : null} обрав кількість команд: ${store.teamsQuantity}</i>
+<i>ℹ️ ${first_name} ${last_name ? last_name : username} обрав кількість команд: ${store.teamsQuantity}</i>
 ${replies.sendPlayersListReply}
 `
     if (store.splitVariant === 'skill_split') {
-      reply = `${reply}${replies.youChoseSkillSplitReply}`;
+      reply = `${reply}${replies.youChoseSkillSplitReply}`
     }
 
-    await ctx.replyWithHTML(reply);
+    await ctx.replyWithHTML(reply)
   } catch (err) {
-    await handleError(err, ctx);
+    await handleError(err, ctx)
   }
-};
+}
