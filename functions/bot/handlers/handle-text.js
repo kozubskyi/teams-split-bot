efed295293127f853c432612a358fa399ed8feba
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2022-12-27 14:11:21 +0200

    handle all variants of players list

--- functions/bot/handlers/handle-text.js
+++ functions/bot/handlers/handle-text.js
@@ -1,28 +1,37 @@
-const { store, resetStore } = require('../store')
-const helpers = require('../helpers')
-const { buttons } = require('../helpers')
-const textHandlers = require('./text-handlers')
-const splitHandlers = require('./split-handlers')
-const handleError = require('./handle-error')
+const { store, resetStore } = require('../store');
+const helpers = require('../helpers');
+const { buttons } = require('../helpers');
+const textHandlers = require('./text-handlers');
+const splitHandlers = require('./split-handlers');
+const handleError = require('./handle-error');
 
 module.exports = async function handleText(ctx) {
   try {
-    if (!store.splitVariant || !store.teamsQuantity) return
+    if (!store.splitVariant || !store.teamsQuantity) return;
 
-    store[store.list] = ctx.message.text.split('\n').map((playerString) => {
-      if (Number.isNaN(parseInt(playerString))) return playerString
+    const splitSymbols = ['.', ')', '-'];
 
-      const playerArray = playerString.split('.')
-      return playerArray.length === 1 ? playerArray[0].trim() : playerArray[1].trim()
-    })
+    store[store.list] = ctx.message.text.split('\n').map(playerString => {
+      for (let i = 0; i < playerString.length; i++) {
+        if (
+          splitSymbols.includes(playerString[i]) &&
+          (!Number.isNaN(Number(playerString[i - 1])) || !Number.isNaN(Number(playerString[i - 2])))
+        ) {
+          playerString[i] = '~';
+        }
+      }
+
+      const playerArray = playerString.split('~');
+      return playerArray.length === 1 ? playerArray[0].trim() : playerArray[1].trim();
+    });
 
     if (store.list === 'captains') {
-      await textHandlers.handleCaptains(ctx)
+      await textHandlers.handleCaptains(ctx);
     }
     if (store.list === 'players') {
-      await textHandlers.handlePlayers(ctx)
+      await textHandlers.handlePlayers(ctx);
     }
   } catch (err) {
-    await handleError(err, ctx)
+    await handleError(err, ctx);
   }
-}
+};
