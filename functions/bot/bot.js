7fb051e1714349ca969808ad14a8eb65ed8d6234
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-02-15 14:17:17 +0200

    bugfix

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -41,19 +41,19 @@ function start() {
 
 start()
 
-bot.launch()
+// bot.launch()
 
 // Enable graceful stop
 // process.once('SIGINT', () => bot.stop('SIGINT'))
 // process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-// exports.handler = async event => {
-// 	try {
-// 		await bot.handleUpdate(JSON.parse(event.body))
-// 		return { statusCode: 200, body: '' }
-// 	} catch (e) {
-// 		console.error('error in handler:', e)
-// 		return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' }
-// 	}
-// }
+exports.handler = async event => {
+	try {
+		await bot.handleUpdate(JSON.parse(event.body))
+		return { statusCode: 200, body: '' }
+	} catch (e) {
+		console.error('error in handler:', e)
+		return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' }
+	}
+}
