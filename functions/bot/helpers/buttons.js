b503cfee2fb884570ae3636c79185cd35970d786
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-04-18 14:06:30 +0300

    cahnge button icons in skill_split and change_captains

--- functions/bot/helpers/buttons.js
+++ functions/bot/helpers/buttons.js
@@ -12,13 +12,13 @@ const {
 
 const buttons = Object.freeze({
 	CAPTAINS_SPLIT_BUTTON: Markup.button.callback(`👥 ${CAPTAINS_SPLIT}`, CAPTAINS_SPLIT),
-	SKILL_SPLIT_BUTTON: Markup.button.callback(`📊 ${SKILL_SPLIT}`, SKILL_SPLIT), // 📊💪
+	SKILL_SPLIT_BUTTON: Markup.button.callback(`💪 ${SKILL_SPLIT}`, SKILL_SPLIT), // 📊💪
 	RANDOM_SPLIT_BUTTON: Markup.button.callback(`🎲 ${RANDOM_SPLIT}`, RANDOM_SPLIT),
 
 	RANDOM_CAPTAINS_BUTTON: Markup.button.callback(`👨‍✈️ ${RANDOM_CAPTAINS}`, RANDOM_CAPTAINS), // 👨‍✈️
 	CANCEL_LAST_CHOICE_BUTTON: Markup.button.callback(`❌ ${CANCEL_LAST_CHOICE}`, CANCEL_LAST_CHOICE), // ❌✖️🚫❎
 	CHANGE_SEQUENCE_BUTTON: Markup.button.callback(`🔙 ${CHANGE_SEQUENCE}`, CHANGE_SEQUENCE), // ↩️🔙🔀
-	CHANGE_CAPTAINS_BUTTON: Markup.button.callback(`©️ ${CHANGE_CAPTAINS}`, CHANGE_CAPTAINS), // ©️⭐️👨‍✈️👥
+	CHANGE_CAPTAINS_BUTTON: Markup.button.callback(`👨‍✈️ ${CHANGE_CAPTAINS}`, CHANGE_CAPTAINS), // ©️⭐️👨‍✈️👥
 	// RESPLIT_WITH_THE_CAPTAINS_BUTTON: Markup.button.callback(`©️ ${RESPLIT_WITH_THE_CAPTAINS}`, RESPLIT_WITH_THE_CAPTAINS), // ©️⏮⏪◀️⬅️🔚🔙🔝
 
 	BACK_TO_QUESTIONS_BUTTON: Markup.button.callback(BACK_TO_QUESTIONS, BACK_TO_QUESTIONS),
