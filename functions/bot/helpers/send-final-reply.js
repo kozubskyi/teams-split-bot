f1fcffb8800e77ac8741b3cf48b00b261e8a63c1
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-09-22 15:44:15 +0300

    додав кнопку Трансфери

--- functions/bot/helpers/send-final-reply.js
+++ functions/bot/helpers/send-final-reply.js
@@ -1,6 +1,8 @@
-const { resetStore } = require('../services/stores-api')
+const { Markup } = require('telegraf')
+// const { resetStore } = require('../services/stores-api')
 const getLineups = require('./get-lineups')
 const sendInfoMessageToCreator = require('./send-info-message-to-creator')
+const { TRANSFERS_BUTTON } = require('./buttons')
 
 module.exports = async function sendFinalReply(ctx, { splitVariant, teamsQuantity, teamsData }) {
 	const reply = `
@@ -8,7 +10,9 @@ module.exports = async function sendFinalReply(ctx, { splitVariant, teamsQuantit
 Варіант розподілу: ${splitVariant}
 Кількість команд: ${teamsQuantity} ${getLineups(teamsData)}`
 
-	await ctx.replyWithHTML(reply)
+	const buttons = Markup.inlineKeyboard([[TRANSFERS_BUTTON]])
+
+	await ctx.replyWithHTML(reply, buttons)
 	await sendInfoMessageToCreator(ctx, reply)
-	await resetStore(ctx)
+	// await resetStore(ctx)
 }
