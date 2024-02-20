9568259fb7f0a16f428cbefb56a5bef4ca47f237
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-02-20 19:03:44 +0200

    update version command

--- functions/bot/handlers/handle-version-command.js
+++ functions/bot/handlers/handle-version-command.js
@@ -1,20 +1,12 @@
-const fs = require('fs')
-const path = require('path')
 const { handleChat } = require('../services/chats-api')
-const handleError = require('./handle-error')
 const sendInfoMessageToCreator = require('../helpers/send-info-message-to-creator')
+const handleError = require('./handle-error')
 
-module.exports = async function handleVersionCommand(ctx) {
+module.exports = function handleVersionCommand(ctx) {
 	try {
-		await handleChat(ctx)
+		handleChat(ctx)
 
-		fs.readFile(path.resolve(__dirname, '../../../package.json'), 'utf8', (err, data) => {
-			if (err) return handleError({ ctx, err })
-
-			const { version, lastUpdateDate } = JSON.parse(data)
-
-			ctx.replyWithHTML(`<b>Версія: ${version}</b>\nОновлено: ${lastUpdateDate}`)
-		})
+		ctx.replyWithHTML('<b>Версія: 3.4.3</b>\nОновлено: 20.02.2024')
 
 		sendInfoMessageToCreator(ctx)
 	} catch (err) {
